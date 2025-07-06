7905e1205df3655885f38e67eb64d9d6dbdd1aa6
@Khushi21 <bhopishrutika21@gmail.com>
2025-07-06 22:04:30 +0100

    app updated

--- sample-application/app.py
+++ sample-application/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Azure + Docker + Ansible!"
+    return "Hello this is  our  Network And System Administration Project"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
