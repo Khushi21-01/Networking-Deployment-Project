7b95352049f10367b5de73fb61d5abed7ee55937
@Khushi21 <bhopishrutika21@gmail.com>
2025-07-07 16:39:25 +0100

    error solving

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,6 +32,36 @@ resource "azurerm_public_ip" "public_ip" {
   allocation_method   = "Static"
   sku                 = "Standard"
 }
+resource "azurerm_network_security_group" "ssh_nsg" {
+  name                = "ssh-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
 
 
 resource "azurerm_network_interface" "nic" {
