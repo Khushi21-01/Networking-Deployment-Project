d6b64a599b8ad320efc8ae20d352820fa0127725
@Khushi21 <bhopishrutika21@gmail.com>
2025-07-08 11:03:12 +0100

    test

--- sample-application/app.py
+++ sample-application/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello this is  our  Network And System Administration Project"
+    return "Hello this is  our  Network And System Administration Project , now this is deployment test"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
